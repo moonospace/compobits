35e55ec425ed838cdb0134ece4057cac91921421
Nyoman Sunima <nyomansunima@gmail.com>
2025-11-29 00:41:56 +0800

    feat: add codebase

---
+++ src/features/landing/hero-section.tsx
@@ -0,0 +1,97 @@
+export function HeroSection() {
+  return (
+    <section className="container mx-auto flex flex-col items-center px-5 pt-28 lg:px-0">
+      <div className="flex">
+        <svg
+          fill="none"
+          height="100"
+          viewBox="0 0 280 280"
+          width="100"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M261.333 114.495L114.27 261.304C118.306 262.061 122.342 262.818 126.378 263.322L263.099 126.603C262.847 122.567 262.09 118.531 261.333 114.495Z"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2.52252"
+          />
+          <path
+            d="M252.757 89.0181L89.0452 252.983C92.3244 254.496 95.6037 255.757 99.1353 257.019L257.045 99.1081C255.784 95.8289 254.27 92.2973 252.757 89.0181Z"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2.52252"
+          />
+          <path
+            d="M228.036 53.4505C227.027 52.4415 225.766 51.4324 224.505 49.9189L49.946 224.478C51.2072 225.991 52.4685 227 53.4775 228.009C54.4865 229.018 55.7478 230.027 57.009 231.541L231.568 56.9817C230.054 55.4682 229.045 54.4595 228.036 53.4505Z"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2.52252"
+          />
+          <path
+            d="M228.037 228.008C239.388 216.657 248.216 203.792 254.27 189.161L189.19 254.241C203.568 248.187 216.685 239.358 228.037 228.008Z"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2.52252"
+          />
+          <path
+            d="M161.189 262.316L262.342 161.162C263.099 155.865 263.856 150.567 263.856 145.522L145.297 264.082C150.846 263.829 155.892 263.325 161.189 262.316Z"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2.52252"
+          />
+          <path
+            d="M182.125 24.4399L24.2156 182.35C25.4768 185.629 26.7381 189.16 28.2516 192.44L191.963 28.7282C188.936 26.9625 185.657 25.7012 182.125 24.4399Z"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2.52252"
+          />
+          <path
+            d="M119.819 18.6392L18.4138 119.792C17.4048 125.594 16.9003 131.144 16.6481 136.693L136.72 16.6211C131.171 17.1256 125.369 17.6302 119.819 18.6392Z"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2.52252"
+          />
+          <path
+            d="M53.2238 53.4505C41.8724 64.8018 33.0436 77.6664 26.9896 92.2971L92.0707 27.2162C77.6923 33.2703 64.5752 42.0991 53.2238 53.4505Z"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2.52252"
+          />
+          <path
+            d="M205.081 35.2888L35.3157 205.054C37.0815 208.081 39.0995 210.856 40.8653 213.379L213.406 40.8384C210.883 39.0726 208.109 37.3068 205.081 35.2888Z"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2.52252"
+          />
+          <path
+            d="M154.63 18.1337L17.9094 154.854C18.4139 158.89 18.9184 162.926 19.9274 166.963L166.738 20.1517C162.954 19.1427 158.666 18.6382 154.63 18.1337Z"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2.52252"
+          />
+          <path
+            d="M240.144 67.8279L67.6036 240.367C70.3784 242.385 73.1531 244.151 75.9279 245.917L245.694 76.1522C244.18 73.3774 242.162 70.3504 240.144 67.8279Z"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2.52252"
+          />
+          <path
+            d="M240.145 84.9828L93.3339 231.794C97.37 232.551 101.406 233.307 105.442 233.812L242.163 97.0909C241.658 93.0548 240.902 89.0188 240.145 84.9828Z"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="2.52252"
+          />
+        </svg>
+      </div>
+
+      <h2 className="mt-10 text-center font-medium text-5xl tracking-tighter">
+        Compobits.
+      </h2>
+      <p className="mt-6 text-pretty text-center text-foreground/80 leading-relaxed lg:w-5/12">
+        Stunning crafted react components collections that built with motion to
+        speed up dev and used with copy and paste. Built on top of react, motion
+        and others.
+      </p>
+    </section>
+  );
+}
